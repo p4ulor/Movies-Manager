07086ab43e6713e739f54527c16038f453cf1a14
cteixeira1972 <A25918@alunos.isel.pt>
2022-11-07 23:08:21 +0000

    Server
    Server initial

---
+++ Code/cmdb-server.js
@@ -0,0 +1,27 @@
+// Application Entry Point. 
+// Register all HTTP API routes and starts the server
+
+import express from 'express'
+import * as api from './cmdb-web-api.mjs'
+
+const PORT = 1904
+
+console.log("Start setting up server")
+let app = express()
+
+app.use(express.json())
+
+app.get('/movie', api.getMoviesList)
+app.get('/movie/:name', api.getMovieByName)
+app.post('/group/:name:description', api.createGroup)
+app.put('/group/:id:name:description', api.updateGroup)
+app.get('/group', api.getGroupList)
+app.delete('/group/:id', api.deleteGroup)
+app.post('/group/:idGroup:idMovie', api.addMovieToGroup)
+app.delete('/group/:idGroup:idMovie', api.deleteMovieFromGroup)
+
+app.listen(PORT, () => console.log(`Server listening in http://localhost:${PORT}`))
+
+console.log("End setting up server")
+
+// Route handling functions
